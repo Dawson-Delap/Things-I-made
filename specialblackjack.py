55a1de54ef8e6f530925f7089aefad9d473a5e77
Dawson-Delap <dawsondelap97@gmail.com>
2024-08-22 14:31:42 -0700

    Update specialblackjack.py

--- specialblackjack.py
+++ specialblackjack.py
@@ -129,7 +129,8 @@ stop = 0
 dealcol = 3
 winer = 0
 loser = 0
-def hits(here):
+def resta():
+    window.destroy()
     global numplay
     global cards
     global deal
@@ -140,19 +141,45 @@ def hits(here):
     global hidendeal
     global hidennum
     global stop
-    if here == 1:
-        start.destroy()
-        hit = Button(window, text="Hit", font=("Arial", 25), width=4, height=1, bg="gold", command=lambda: hits(0)).grid(row=2,column=0)
-        pas = Button(window, text="pass", font=("Arial", 25), width=4, height=1, bg="gold", command=lambda: passs(0)).grid(row=2,column=2)
-        deal = random.choice(list(cards.keys()))
-        hidendeal = random.choice(list(cards.keys()))
-        dealer["text"] = deal
-        dealer["fg"] = colors[deal]
-        numdeal = cards[deal]
-        print(hidendeal)
-        dealnum["text"] = numdeal
-        cards.pop(deal)
-        win()
+    global dealcol
+    global winer
+    global loser
+    deal = 0
+    play = 0
+    colnum = 3
+    numplay = 0
+    hidendeal = 0
+    stopper = 0
+    numdeal = 0
+    hidennum = 0
+    stop = 0
+    dealcol = 3
+    winer = 0
+    loser = 0
+    windows()
+def deals():
+    global numplay
+    global cards
+    global deal
+    global play
+    global colnum
+    global stopper
+    global numdeal
+    global hidendeal
+    global hidennum
+    global stop
+    start.destroy()
+    hit = Button(window, text="Hit", font=("Arial", 25), width=4, height=1, bg="gold", command=lambda: another()).grid(row=2,column=0)
+    pas = Button(window, text="pass", font=("Arial", 25), width=4, height=1, bg="gold", command=lambda: passs(0)).grid(row=2,column=2)
+    deal = random.choice(list(cards.keys()))
+    hidendeal = random.choice(list(cards.keys()))
+    dealer["text"] = deal
+    dealer["fg"] = colors[deal]
+    numdeal = cards[deal]
+    print(hidendeal)
+    dealnum["text"] = numdeal
+    cards.pop(deal)
+    win()
     print("runs")
     play = random.choice(list(cards.keys()))
     play2 = random.choice(list(cards.keys()))
@@ -165,8 +192,9 @@ def hits(here):
     cards.pop(play2)
     card = Button(window, text=play2, fg=colors[play2], font=("Arial", 25), width=8, height=8).grid(row=1, column=2)
     win()
-    if here == 0:
-        another()
+
+
+
 def passs(rev):
     global numdeal
     global dealcol
@@ -187,13 +215,23 @@ def passs(rev):
     dealcol += 1
     win()
     passwin()
+
+
+
 def another():
     global colnum
+    global numplay
+    global playnum
     another1 = random.choice(list(cards.keys()))
-    cards.pop(another1)
-    card = Button(window, text=another1, fg=colors[another1], font=("Arial", 25), width=4, height=4).grid(row=1, column=colnum)
     colnum += 1
+    numplay += cards[another1]
+    playnum["text"] = numplay
+    card = Button(window, text=another1, fg=colors[another1], font=("Arial", 25), width=4, height=4).grid(row=1, column=colnum)
+    cards.pop(another1)
     win()
+
+
+
 def win():
     global loser
     global winer
@@ -222,18 +260,32 @@ def passwin():
         w = Label(wine, text= "YOU WON :)", font=("Arial", 100))
         w.pack()
         wine.mainloop()
-window = Tk()
-window.config(bg="darkgreen")
-dealer = Button(window, text="dealer", font=("Arial", 25), width=8, height=8)
-dealer.grid(row=0, column=1)
-hide = Button(window, text="hidden", font=("Arial", 25), width=8, height=8)
-hide.grid(row=0, column=2)
-player = Button(window, text="player", font=("Arial", 25), width=8, height=8)
-player.grid(row=1, column=1)
-start = Button(window, text="Start", font=("Arial", 25), width=4, height=1, bg="gold",  command=lambda:  hits(1))
-start.grid(row=2, column=1)
-playnum = Button(window, text="0", font=("Arial", 25), bg="gold")
-playnum.grid(row=1, column=0)
-dealnum = Button(window, text="0", font=("Arial", 25), bg="gold")
-dealnum.grid(row=0, column=0)
-window.mainloop()
+
+
+def windows():
+    global playnum
+    global dealnum
+    global hide
+    global window
+    global start
+    global player
+    global dealer
+    window = Tk()
+    window.config(bg="darkgreen")
+    dealer = Button(window, text="dealer", font=("Arial", 25), width=8, height=8)
+    dealer.grid(row=0, column=1)
+    hide = Button(window, text="hidden", font=("Arial", 25), width=8, height=8)
+    hide.grid(row=0, column=2)
+    player = Button(window, text="player", font=("Arial", 25), width=8, height=8)
+    player.grid(row=1, column=1)
+    start = Button(window, text="Start", font=("Arial", 25), width=4, height=1, bg="gold",  command=lambda:  deals())
+    start.grid(row=2, column=1)
+    playnum = Button(window, text="0", font=("Arial", 25), bg="gold")
+    playnum.grid(row=1, column=0)
+    dealnum = Button(window, text="0", font=("Arial", 25), bg="gold")
+    dealnum.grid(row=0, column=0)
+    restart = Button(window, text="Restart", font=("Arial", 25), bg="gold", command=lambda:  resta())
+    restart.grid(row=3, column=1)
+    window.mainloop()
+
+windows()
