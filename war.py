0c2f26e4ea499c5155dfa12a31db5ae8692112d3
Dawson-Delap <dawsondelap97@gmail.com>
2024-08-23 13:46:36 -0700

    Update war.py

--- war.py
+++ war.py
@@ -260,19 +260,30 @@ def deal(Event=None):
         if botnum > playnum and stop == 0:  
             stop = 1 
             lose = Tk()
-            wol = Label(lose, text="YOU LOSE!", font=("Arial", 75))
+            lose.config(bg="darkgreen")
+            wol = Label(lose, text="YOU LOSE!", font=("Arial", 75), bg="darkgreen", fg="white")
             wol.grid(row=0, column=0)
-            restart = Button(lose, text="Restart?", font=("Arial", 25), command=lambda: start())
+            restart = Button(lose, text="Restart?", font=("Arial", 25), bg="gold", command=lambda: start())
             restart.grid(row=1, column=0)
             lose.mainloop()
         elif playnum > botnum and stop == 0: 
             stop = 1 
             lose = Tk()
+            lose.config(bg="darkgreen")
             wol = Label(lose, text="YOU WIN!", font=("Arial", 75))
             wol.grid(row=0, column=0)
-            restart = Button(lose, text="Restart?", font=("Arial", 25), command=lambda: start())
+            restart = Button(lose, text="Restart?", font=("Arial", 25), bg="gold", command=lambda: start())
             restart.grid(row=1, column=0)
             lose.mainloop()
+        elif stop == 0:
+            stop = 1
+            lose = Tk()
+            lose.config(bg="darkgreen")
+            wol = Label(lose, text="TIED!", font=("Arial", 75))
+            wol.grid(row=0, column=0)
+            restart = Button(lose, text="Restart?", font=("Arial", 25), bg="gold", command=lambda: start())
+            restart.grid(row=1, column=0)
+            lose.mainloop() 
 def botdeal():
     global botcard
     global botnum
@@ -282,6 +293,7 @@ def botdeal():
         botcard = random.choice(list(cards.keys()))
         botcardnum = cards[botcard]
         bot["text"] = botcard
+        bot["fg"] = colors[botcard]
         cards.pop(botcard)
 def playdeal():
     global playcard
@@ -292,6 +304,7 @@ def playdeal():
         playcard = random.choice(list(cards.keys()))
         playcardnum = cards[playcard]
         player["text"] = playcard
+        player["fg"] = colors[playcard]
         cards.pop(playcard)
 def award():
     global botnum
@@ -312,15 +325,16 @@ def windows():
     global bot
     global window
     window = Tk()
+    window.config(bg="darkgreen")
     bot = Button(window, text="bot", font=("Arial", 25), height=8, width=9)
     bot.grid(row=0, column=1)
-    player = Button(window, text="player", font=("Arial", 25), height=8, width=9)
+    player = Button(window, text="player", font=("Arial", 25), height=8, width=9,)
     player.grid(row=1, column=1)
-    botnumbut = Button(window, text=botnum, font=("Arial", 25))
+    botnumbut = Button(window, text=botnum, font=("Arial", 25), bg="gold")
     botnumbut.grid(row=0, column=0)
-    playnumbut = Button(window, text=playnum, font=("Arial", 25))
+    playnumbut = Button(window, text=playnum, font=("Arial", 25), bg="gold")
     playnumbut.grid(row=1, column=0)
-    startbut = Button(window, text=playnum, font=("Arial", 25), command=lambda: deal())
+    startbut = Button(window, text="deal", font=("Arial", 25), bg="gold", command=lambda: deal())
     startbut.grid(row=2, column=1)
     window.bind('<space>', deal)
     window.mainloop()
