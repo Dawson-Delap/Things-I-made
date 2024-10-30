afcaa4c6e2c4724e8d2ea7d75e925d39169fd9f2
Dawson Delap <dawsondelap97@gmail.com>
2024-10-30 13:09:25 -0700

    Update specialblackjack.py

--- specialblackjack.py
+++ specialblackjack.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import random
-
+#!!!!!!!!!!!!!!!!!!!!!!!!!!DOES NOT WORK VERY WELL: DOES NOT HAVE ACE RULE AND DEALER IS NOT FUNCTIONING LIKE REAL BLACKJACK IT IS ONLY SIMILAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 cards = {
         #clubs
          '2♣':2,'3♣':3,'4♣':4,'5♣':5,'6♣':6,'7♣':7,'8♣':8,'9♣':9,'10♣':10,'J♣':10,'Q♣':10,'K♣':10,'A♣':11,
